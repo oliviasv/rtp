50a6e31985c4ffd8a0a9744f18560ee0aec6597a
olivia <olivia.savitchi@isa.utm.md>
2021-02-21 12:47:57 +0200

    small changes to initial commit

---
+++ lab_1/lab1/lib/lab1/application.ex
@@ -0,0 +1,20 @@
+defmodule Lab1.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Lab1.Worker.start_link(arg)
+      # {Lab1.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Lab1.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
